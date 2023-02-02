ff7f75bb56f077fb437a1c3bd6d3358b5051071e
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-01 22:03:24 -0800

    add branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 // User Interface Logic
 
+window.onload = function() {
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+
+    function hideResults() {
+      document.getElementById("swings").setAttribute("class", "hidden");
+      document.getElementById("coaster").setAttribute("class", "hidden");
+      document.getElementById("tower").setAttribute("class", "hidden");
+      document.getElementById("sorry").setAttribute("class", "hidden");
+      hideResults();
+      const age = parseInt(document.querySelector("input#age").value);
+      const height = parseInt(document.querySelector("input#height").value);
+   }
+  };
+};
+
+
 function hideResults() {
   document.getElementById("swings").setAttribute("class", "hidden");
   document.getElementById("coaster").setAttribute("class", "hidden");
   document.getElementById("tower").setAttribute("class", "hidden");
   document.getElementById("sorry").setAttribute("class", "hidden");
 }
-
-window.onload = function() {
-  document.querySelector("form").onsubmit = function(event) {
-    event.preventDefault();
-    hideResults();
-    const age = parseInt(document.querySelector("input#age").value);
-    const height = parseInt(document.querySelector("input#height").value);
-  };
-};
